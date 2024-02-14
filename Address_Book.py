e9311e8f696b465494ea12155ce739a44924026f
Mayank Mudgal <mayankmudgal111+github@gmail.com>
2024-02-14 11:39:17 +0530

    [Address Book] use case 5 solved

--- Address_Book.py
+++ Address_Book.py
@@ -22,12 +22,16 @@ class AddressBook:
   self.contacts = {}
  
  def add(self, contact):
-  self.contacts[contact["fname"]] = contact
+  if contact["fname"] not in self.contacts:
+    self.contacts[contact["fname"]] = contact
+  else:
+   print("Contact already Exists")
  
  def edit_contact(self, fname, att, value)->bool:
   if fname in self.contacts and self.contacts[fname][att]:
    self.contacts[fname][att] = value
    print(fname + "'s Contact was Edited")
+   print(self.contacts[fname])
   else:
    print(fname + " Contact NOT found")
   # O(1) Time Complexity
@@ -45,12 +49,12 @@ class AddressBook:
 
 
 class Contact:
- def __init__(self, fname, lname = ""):
+ def __init__(self, fname, lname, address, city):
   self.detail = {}
   self.detail["fname"] = fname
   self.detail["lname"] = lname
-  self.detail["address"] = "Sec 10A Gurgaon"
-  self.detail["city"] = "Gurgaon"
+  self.detail["address"] = address
+  self.detail["city"] = city
   self.detail["state"] = "Haryana"
   self.detail["zip"] = "122001"
   self.detail["ph"] = "7290833669"
@@ -71,24 +75,27 @@ class Contact:
    return False
   
  def __str__(self) -> str:
-  return self.detail["fname"] + " " + self.detail["lname"] + " " + self.detail["address"] 
+  return self.detail["fname"] + " " + self.detail["lname"] + " " + self.detail["address"] + " " + self.detail["city"]
  # + " " + self.detail["state"] +  " " + self.detail["ph"] 
  # + " " + self.detail["email"]
   
-p1 = Contact("Mayank", "Mudgal")
-p2 = Contact("Sucre")
-p3 = Contact("Sarah", "Tancredi")
-p4 = Contact("Michael", "Scofield")
-p5 = Contact("Lincon", "Burrows")
-p6 = Contact("T", "Bag")
+p1 = Contact("Mayank", "Mudgal", "3867 Treutel Greens", "LA")
+p2 = Contact("Sucre", "", "49 Paucek Lane", "Detroit")
+p3 = Contact("Sarah", "Tancredi", "rue Van Hecke 47", "Torhout")
+p4 = Contact("Michael", "Scofield", "61642 Zulauf Plaza", "Wisconsin")
+p5 = Contact("Lincon", "Burrows", "Studio 66 Miller Parkways", "London")
+p6 = Contact("T", "Bag", "5455 Bauch Pine Suite 150", "East Raheemborough")
+print(p1["fname"])
 book = AddressBook()
 book.add(p1)
 book.add(p2)
 book.add(p3)
 book.add(p4)
+book.add(p1)
 book.add(p5)
 book.add(p6)
-book.edit_contact("Mayank", "city", "sec 122")
+book.edit_contact("Mayank", "city", "South Park")
 book.delete("Sarah")
-book.display()
+# book.display()
+
 
